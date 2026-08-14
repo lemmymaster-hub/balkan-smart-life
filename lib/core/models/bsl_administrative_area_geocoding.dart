c6ba63c8174c2d6e67762c083c1942661d5e427e
lemmymaster-hub <lemmymaster@gmail.com>
2026-08-14 14:41:02 +0200

    refactor(location): reuse administrative entity qualifier

--- lib/core/models/bsl_administrative_area_geocoding.dart
+++ lib/core/models/bsl_administrative_area_geocoding.dart
@@ -2,13 +2,7 @@ import 'bsl_administrative_area.dart';
 
 extension BslAdministrativeAreaGeocoding on BslAdministrativeArea {
   String get geocodingQuery {
-    switch (entity) {
-      case BslAdministrativeEntity.federation:
-        return '$geocodingName, Federacija Bosne i Hercegovine';
-      case BslAdministrativeEntity.republikaSrpska:
-        return '$geocodingName, Republika Srpska';
-      case BslAdministrativeEntity.brcko:
-        return '$geocodingName, Brčko distrikt';
-    }
+    final qualifier = BslAdministrativeAreas.entityQualifier(this);
+    return '$geocodingName, $qualifier';
   }
 }
